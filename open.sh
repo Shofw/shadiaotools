0839ffff633c1ce4dae07bd16f3ece5b0588d730
Ma Ping <3207754367@qq.com>
2019-06-27 02:12:50 +0800

    apk_file

--- open.sh
+++ open.sh
@@ -1,70 +1,197 @@
-#!/bin/sh
-
+#!/bin/bash
 #菜单
 menu (){
 clear
 echo  "
-快速打开一些隐藏活动
 
+[快速打开一些隐藏活动]
 0.电池优化
 1.Android Pie电池管理器
 2.开启极致神隐
 3.应用和通知
-4.通知日志
 
-快速冻结/卸载MIUI广告及系统组件
-5.卸载MIUI组件
-6.冻结MIUI组件
-7.解冻MIUI组件
-8.显示即将去世的组件列表
-9.退出脚本
+[冻结/卸载MIUI组件]
+4.卸载MIUI组件
+5.冻结MIUI组件
 
-"$list"
-	
-Please enter the number:"
-read  open
+6.adb激活快捷工具
+[类原生工具]
+7.快速安装APP
+8.去除x号
+9.跳过Google验证 (twrp下请先手动挂载system读写) 
+
+按回车键退出脚本.
+"
+read -p "Please enter the number:" open
 case  $open in
 	
 0)
+
 i='com.android.settings/com.android.settings.Settings$HighPowerApplicationsActivity'
 am start -n $i
+sleep 1 &&  menu
 	;;
 
 1)
 u='com.android.settings/com.android.settings.Settings$PowerUsageSummaryActivity'
 p=`getprop ro.product.brand` ; [ $p = xiaomi ] && u='com.android.settings/com.android.settings.fuelgauge.PowerUsageSummary'
 am start -n $u
+sleep 1 &&  menu
 	;;
 
 2)
 i='com.miui.powerkeeper/com.miui.powerkeeper.ui.ExtremePowerEntryActivity'
 am start -n $i
+sleep 1 &&  menu
 	;;
 3)
 i='com.android.settings/com.android.settings.Settings$AppAndNotificationDashboardActivity'
 am start -n $i
+sleep 1 &&  menu
 	;;
-
 4)
-i='com.android.settings/com.android.settings.Settings$NotificationStationActivity'
-am start -n $i
+pm uninstall --user 0 com.miui.video
+pm uninstall --user 0 com.miui.player
+pm uninstall --user 0 com.mipay.wallet
+pm uninstall --user 0 com.xiaomi.payment
+pm uninstall --user 0 com.xiaomi.gamecenter.sdk.service
+pm uninstall --user 0 com.miui.analytics
+pm uninstall --user 0 com.xiaomi.ab
+pm uninstall --user 0 com.miui.systemAdSolution
+sleep 1 &&  menu
 	;;
 5)
-pm uninstall --user 0 com.miui.video ; pm uninstall --user 0 com.miui.player ;pm uninstall --user 0 com.mipay.wallet ; pm uninstall --user 0 com.xiaomi.payment ; pm uninstall --user 0 com.xiaomi.gamecenter.sdk.service ; pm uninstall --user 0 com.miui.analytics ; pm uninstall --user 0 com.xiaomi.ab ; pm uninstall --user 0 com.miui.systemAdSolution 
+pm disable-user com.miui.video
+pm disable-user com.miui.player
+pm disable-user com.miui.analytics
+pm disable-user com.xiaomi.ab
+pm disable-user com.xiaomi.payment
+pm diasble-user com.xiaomi.gamecenter.sdk.service
+pm disable-user com.xiaomi.payment
+pm disable-user com.mipay.wallet
+pm disable-user com.miui.systemAdSolution
+sleep 1 &&  menu
 	;;
 6)
-pm disable-user com.miui.video ;pm disable-user com.miui.player ; pm disable-user com.miui.analytics ; pm disable-user com.xiaomi.ab ; pm disable-user com.xiaomi.payment ; pm diasble-user com.xiaomi.gamecenter.sdk.service ; pm disable-user com.xiaomi.payment ; pm disable-user com.mipay.wallet ; pm disable-user com.miui.systemAdSolution
-	;;
-7)
-pm enable com.miui.video ; pm enable com.miui.player ; pm enable com.xiaomi.payment ; pm enable com.xiaomi.gamecenter.sdk.service ; pm enable com.xiaomi.payment ; pm enable com.miui.analytics ; pm enable com.xiaomi.ab ; pm enable com.mipay.wallet ; pm enable com.miui.systemAdSolution
+adbtools #adb快捷启动
+sleep 1 &&  menu
         ;;
+7)
+cp apk/* /data/local/tmp/ 
+cd /data/local/tmp/ 
+install_apk #启动安装方法
+	;;
 8)
-list="小米视频 小米音乐 小米钱包 游戏服务组件 米币支付 analytics mab msa"
-;;
+#删除验证服务器
+settings delete global captive_portal_https_url
+settings delete global captive_portal_http_url
+#重新设置验证服务器
+settings put global captive_portal_http_url http://captive.v2ex.co/generate_204
+settings put global captive_portal_https_url https://captive.v2ex.co/generate_204
+
+sleep 1 &&  menu
+	;;
 9)
+#跳过Google验证
+mount -o rw,remount /system
+echo ro.setupwizard.mode=DISABLED >> /system/build.prop
+dd if=/dev/zero of=/dev/block/bootdevice/by-name/splash
+sleep 1 &&  menu
+	;;
+*)
 	exit
 esac
-menu
+
 }
 
+adbtools (){
+clear
+echo "
+1.激活冰箱
+2.激活shizuku
+3.激活黑域
+4.激活空调狗
+5.adb授权appops所需权限(托管设备管理员模式)
+
+按回车键返回上一级
+"
+read -p "你想激活什么？(键入数字)： " adb_output
+case $adb_output in
+1)
+what_type
+       ;;
+2)
+sh /sdcard/Android/data/moe.shizuku.privileged.api/files/start.sh
+	;;
+3)
+sh /data/data/me.piebridge.brevent/brevent.sh
+	;;
+4)
+dpm set-device-owner me.yourbay.airfrozen/.main.core.mgmt.MDeviceAdminReceiver
+	;;
+5)
+pm grant --user 0 rikka.appops android.permission.GET_APP_OPS_STATS
+	;;
+*)
 menu
+
+esac
+}
+
+what_type (){
+clear
+echo "
+1.普通adb模式
+2.设备管理员模式
+
+按回车键返回上一级
+"
+read -p "请选择激活方式:" what_type
+case $what_type in
+1)
+sh /sdcard/Android/data/com.catchingnow.icebox/files/start.sh
+	;;
+2)
+
+	;;
+*)
+adbtools
+esac
+
+}
+
+install_apk(){
+clear
+echo "
+1.安装钛备份
+2.安装网易云音乐
+3.安装酷安
+4.安装迅雷
+
+回车键返回主菜单
+"
+read -p "你想安装什么？(键入数字)：" output
+case $output in
+
+1)
+input_package="TitaniumBackup.apk"
+	;;
+2)
+input_package="netease.apk"
+	;;
+3)
+input_package="coolapk.apk"
+	;;
+4)
+input_package="xunlei.apk"
+	;;
+*)
+rm -rf /data/local/tmp/*
+sleep 1 && menu
+esac
+pm install -r $input_package
+
+install_apk
+}
+
+menu #启动菜单
