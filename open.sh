64415789bf6a9f10e076d8d69783caaca5cc51d0
ma ping <3207754367@qq.com>
2019-07-20 02:42:55 +0800

    脚本

--- open.sh
+++ open.sh
@@ -194,7 +194,6 @@ pm install -r $input_package
 install_apk
 }
 
-
 Connect_type(){ 
 #环境类型
 clear
@@ -202,20 +201,36 @@ echo "你想使用何种方式运行本脚本？"
 echo "1.root模式"
 echo "2.免root模式(adb)"
 echo "3.运行？段位？"
-echo ""
-read -p "召唤师，请选择您的英雄:" input_type
+echo 
+read -p "好的召唤师，请选择您的英雄:" input_type
 case $input_type in
 1)
-su #提权
-menu #启动菜单
+su -c bash ./open.sh
 ;;
 2)
-#`pip install adb` #安装adb环境
-`adb shell sh ./open.sh` #以adb运行脚本
+adb shell sh ./open.sh
 ;;
 3)
 kill -9 `pgrep -f bash`
 esac
 }
 
+bashisyes(){
+
+clear
+echo 
+echo "欢迎吖！"
+echo
+echo "因本脚本使用bash作为命令解释器，请先前往magisk仓库搜索并安装模块 'GNU' "
+echo
+read -p "若已安装该模块，请按回车键重试."
+if [ -x /bin/bash  ]; then
 Connect_type
+else
+echo
+echo "警告：bash命令解释器不存在！ 请前往magisk仓库安装相关模块"
+fi
+
+}
+
+bashisyes #判断bash是否存在
