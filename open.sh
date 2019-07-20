2cc3af0495f2ca9e8035b50bf6f1f332a75610a0
ma ping <3207754367@qq.com>
2019-07-20 12:31:38 +0800

    巴啦啦拉啦

--- open.sh
+++ open.sh
@@ -191,56 +191,5 @@ pm install -r $input_package
 
 install_apk
 }
-<<<<<<< HEAD
 menu
-=======
 
-Connect_type(){ 
-#环境类型
-clear
-echo "你想使用何种方式运行本脚本？"
-echo "1.root模式"
-echo "2.免root模式(adb)"
-echo "3.运行？段位？"
-echo 
-read -p "好的召唤师，请选择您的英雄:" input_type
-case $input_type in
-1)
-`su && bash open.sh`
-;;
-2)
-echo "请先前往magisk仓库搜索并安装模块 'adb'"
-echo
-read -p "如已安装模块 请按回车键尝试"
-mkdir /sdcard/.android #创建存放adb密钥的目录
-cd /sdcard/.android #进入存放adb密钥的目录
-adb keygen adbkey #生成adb密钥
-adb kill-server #结束adb进程
-cd $HOME/O* #返回工作目录
-`adb shell sh ./open.sh `
-;;
-3)
-kill -9 `pgrep -f bash`
-esac
-}
-
-bashisyes(){
-
-clear
-echo 
-echo "欢迎吖！"
-echo
-echo "因本脚本使用bash作为命令解释器，请先前往magisk仓库搜索并安装模块 'GNU' "
-echo
-read -p "若已安装该模块，请按回车键重试."
-if [ -x /system/xbin/bash  ]; then
-Connect_type
-else
-echo
-echo "警告：bash命令解释器不存在！ 请前往magisk仓库安装相关模块"
-fi
-
-}
-
-bashisyes #判断bash是否存在
->>>>>>> 075f367536a052b8ade6ad319cfe3a5413538cfa
