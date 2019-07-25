21c73163fb579e127871a789d6332dd0e303397d
maping <3207754367@qq.com>
2019-07-25 12:19:07 +0800

    摸仙煲

--- start.sh
+++ start.sh
@@ -5,22 +5,21 @@
 menu(){
 clear
 echo  "
-
 [快速打开一些隐藏活动]
 0.电池优化
 1.Android Pie电池管理器
 2.开启极致神隐
 3.应用和通知
+4.打开自带文档APP
+5.Google设置
 
-[冻结/卸载MIUI组件]
-4.卸载MIUI组件
-5.冻结MIUI组件
-
-6.adb激活快捷工具
+6.
+7.MIUI组件管理
+8.adb激活工具
 
 [类原生工具]
-7.去除x号
-8.跳过Google验证 (twrp下请先手动挂载system读写) 
+9.去除x号
+10.跳过Google验证 (twrp下请先手动挂载system读写)
 
 按回车键退出脚本.
 "
@@ -30,66 +29,55 @@ case  $open in
 0)
 
 i='com.android.settings/com.android.settings.Settings$HighPowerApplicationsActivity'
-am start -n $i
-sleep 1 &&  menu
 	;;
 
 1)
-u='com.android.settings/com.android.settings.Settings$PowerUsageSummaryActivity'
-p=`getprop ro.product.brand` ; [ $p = xiaomi ] && u='com.android.settings/com.android.settings.fuelgauge.PowerUsageSummary'
-am start -n $u
-sleep 1 &&  menu
+i='com.android.settings/com.android.settings.Settings$PowerUsageSummaryActivity'
+p=`getprop ro.product.brand` ; [ $p = xiaomi ] && i='com.android.settings/com.android.settings.fuelgauge.PowerUsageSummary'
+
 	;;
 
 2)
 i='com.miui.powerkeeper/com.miui.powerkeeper.ui.ExtremePowerEntryActivity'
-am start -n $i
-sleep 1 &&  menu
 	;;
 3)
 i='com.android.settings/com.android.settings.Settings$AppAndNotificationDashboardActivity'
-am start -n $i
-sleep 1 &&  menu
 	;;
 4)
-pm uninstall --user 0 $package1
-pm uninstall --user 0 $package2
-pm uninstall --user 0 $package3
-pm uninstall --user 0 $package4
-pm uninstall --user 0 $package5
-pm uninstall --user 0 $package6
-pm uninstall --user 0 $package7
-sleep 1 &&  menu
+i='com.android.documentsui/com.android.documentsui.LauncherActivity'
 	;;
 5)
-package_disable
-sleep 1 &&  menu
+i='com.google.android.gms/com.google.android.gms.app.settings.GoogleSettingsIALink'
 	;;
-6)
-adbtools #adb快捷启动
-sleep 1 &&  menu
+6) echo "想不到吧！这个是空的！ 欧拉欧拉欧拉欧拉欧拉欧拉欧拉欧拉欧拉欧拉欧拉欧拉欧拉欧拉"
 	;;
 7)
+package_miui #MIUI软件包管理
+	;;
+8)
+adbtools #adb快捷启动
+	;;
+9)
+captive_portal=connect.rom.miui.com/generate_204
 #删除验证服务器
 settings delete global captive_portal_https_url
 settings delete global captive_portal_http_url
 #重新设置验证服务器
-settings put global captive_portal_http_url http://captive.v2ex.co/generate_204
-settings put global captive_portal_https_url https://captive.v2ex.co/generate_204
+settings put global captive_portal_http_url http://$captive_portal
+settings put global captive_portal_https_url https://$captive_portal
 
-sleep 1 &&  menu
 	;;
-8)
+10)
 #跳过Google验证
 mount -o rw,remount /system
 echo ro.setupwizard.mode=DISABLED >> /system/build.prop
 dd if=/dev/zero of=/dev/block/bootdevice/by-name/splash
-sleep 1 &&  menu
 	;;
 *)
 	exit
 esac
-
+am start -n $i 
+sleep 1 &&  menu
 }
 
 adbtools (){
@@ -166,33 +154,20 @@ package6=com.xiaomi.gamecenter.sdk.service #小米游戏服务
 package7=com.mipay.wallet #小米钱包
 package8=com.miui.systemAdSolution #广告组件
 
-package_disable (){
+package_miui (){
+echo "以下为去世列表(您只能输入a或b)
 echo
-echo "1.小米视频 2.隐私搜集服务 3.小米音乐 4.米币支付"
-echo "5.小米推广 6.小米游戏服务 7.小米钱包 8.广告组件"
+echo "1.小米视频 2.analytics     3.小米音乐 4.米币支付"
+echo "5.mab     6.小米游戏服务 7.小米钱包 8.msa    "
 echo
-echo "all.全部停用"
+echo "a.全部停用 b.全部卸载"
+echo
 echo "按回车键返回主菜单."
 echo
 
-read -p "你想停用什么？：" output
+read -p "请输入a 或者 b：" output
 case $output in
-
-1) pm disable-user $package1 && package_disable
-	;;
-2) pm disable-user $package2 && package_disable
-	;;
-3) pm disable-user $package3 && package_disable
-	;;
-4) pm disable-user $package4 && package_disable
-	;;
-5) pm disable-user $package5 && package_disable
-	;;
-6) pm disable-user $package6 && package_disable
-	;;
-7) pm disable-user $package7 &&  package_disable
-	;;
-all) 
+a) 
 pm disable-user $package1
 pm disable-user $package2
 pm disable-user $package3
@@ -200,11 +175,20 @@ pm disable-user $package4
 pm disable-user $package5 
 pm disable-user $package6
 pm disable-user $package7
+;;
+b) 
+pm uninstall --user 0 $package1
+pm uninstall --user 0 $package2
+pm uninstall --user 0 $package3
+pm uninstall --user 0 $package4
+pm uninstall --user 0 $package5
+pm uninstall --user 0 $package6
+pm uninstall --user 0 $package7
+;;
 
-sleep 1 && package_disable
-        ;;
 *) menu
 esac
+sleep 1 && package_miui
 }
 
 checkbash(){
@@ -236,27 +220,9 @@ else
 fi
 }
 
-#checkbusybox(){ #检查busybox是否存在
-#if  command -v busybox > /dev/null; then
-#	  checkbash
-#  else
-#	if [ -d "/data/data/com.termux/files/home" ]; then #判断是否存在Termux应用
-#	
-#	ln -s /data/data/com.termux/files/usr/bin/busybox  /bin
-#	#直接创建链接，贼TM方便
-#	else
-#	#木得busybox，还木得termux，玩个毛啊
-#	dd if=/dev/zero of=/sdcard/.nomedia 2>1 >/dev/null &
-#
-#	#小伙子，感受痛苦吧！一袋米要抗几楼!!!!!!
-#	 sleep 1 && checkbash
-#   fi
-#fi
-#}
-
 checkroot() {
 if [ `whoami` = "root" ];then
-        checkbash  #判断busybox是否存在
+        checkbash  #判断bash是否存在
 else
     clear
     echo
